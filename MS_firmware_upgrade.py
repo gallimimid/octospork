0481e820aaccec7fa07436391abe0c8a1944c5e0
gallimimid <gallimimid@gmail.com>
2019-12-03 11:42:18 -0500

    Add files via upload

--- MS_firmware_upgrade.py
+++ MS_firmware_upgrade.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 requests.packages.urllib3.disable_warnings()
 
@@ -17,3 +18,14 @@ response = requests.post(
     )
 
 print(response.status_code,response.headers)
+
+while True:
+    time.sleep(5)
+    response = requests.get(
+        'http://{host}/app_upload.htm'.format(host=ip_address),
+        verify=False,
+        auth=requests.auth.HTTPBasicAuth('admin', 'admin')
+        )
+    print(response.status_code,response.headers)
+    if response.status_code != 200:
+        break
